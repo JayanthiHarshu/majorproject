22b6afa6798b51c2b3bf63f9cb25e424f0797467
JayanthiHarshu <jayanthiharshitha450@gmail.com>
2024-04-15 14:51:41 +0530

    sixth commit

--- visualcrypto/visualcrypto/settings.py
+++ visualcrypto/visualcrypto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ww1ie9d6gjzkxvsw401f96t-s(f(i1#(y7v72w)xt)#2po(f9-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
